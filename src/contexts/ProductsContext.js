47d6a353ec3b8753061898f1a7ea12dc20e4a0f5
tal7aouy <talhaouycoding@gmail.com>
2021-08-04 22:43:50 +0100

    add search product

--- src/contexts/ProductsContext.js
+++ src/contexts/ProductsContext.js
@@ -1,13 +1,25 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext,useEffect,useState } from 'react';
 import { dummyProducts } from '../services/dummy';
 export const ProductsContext = createContext()
 
 const ProductsContextProvider = ({children}) => {
 
     const [products] = useState(dummyProducts);
+    const [filteredProducts, setFilteredProducts] = useState([]);
+
+    const [input,setInput] = useState('')
+    const searchProduct = (name) => {
+        setInput(name)
+    }
+    useEffect(() => {
+        setFilteredProducts(
+            products.filter(product => product.name.toLowerCase().includes(input.toLowerCase()))
+        );
+        console.log("hello")
+    },[input,products])
 
     return ( 
-        <ProductsContext.Provider value={{products}} >
+        <ProductsContext.Provider value={{filteredProducts,searchProduct}} >
             { children }
         </ProductsContext.Provider>
      );
--- src/pages/store/ProductsGrid.js
+++ src/pages/store/ProductsGrid.js
@@ -1,30 +1,33 @@
-import React, { useContext } from 'react';
+import React, {useContext, useState} from 'react';
 import ProductItem from './ProductItem';
 import { ProductsContext } from '../../contexts/ProductsContext';
 import styles from './ProductsGrid.module.scss';
 
 const ProductsGrid = () => {
 
-    const { products} = useContext(ProductsContext)
-
+    const { filteredProducts,searchProduct} = useContext(ProductsContext)
+    const [search,setSearch] = useState('')
+    const handleSearch = (event) => {
+        setSearch(event.target.value)
+        searchProduct(search)
+    }
     return ( 
         <div className={styles.p__container}>
             <div className="row">
                 <div className="col-sm-8">
                     <div className="py-3">
-                        {products.length} Products
+                        {filteredProducts.length} Products
                     </div>
                 </div>
                 <div className="col-sm-4">
                     <div className="form-group">
-                        <input type="text" name="" placeholder="Search product" className="form-control" id=""/>
-                    </div>
+                        <input type="text" name="product_name" onChange={(e)=>handleSearch(e)} placeholder="Search product" className="form-control" id=""/>                    </div>
                 </div>
             </div>
             <div className={styles.p__grid}>
 
                 {
-                    products.map(product => (
+                    filteredProducts.map(product => (
                         <ProductItem key={product.id} product={product}/>
                     ))
                 }
