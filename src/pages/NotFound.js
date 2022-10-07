ca963b136ffa653b1ff2702da3859cbcca1192a7
Juanp <jpse1324@gmail.com>
2022-10-07 07:56:57 -0400

    Made changes to error page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import Layout from '../components/Layout';
+import React from "react";
+import Layout from "../components/Layout";
 
 const NotFound = () => {
-    
-    const center = {
-        textAlign: 'center'
-    }
+  const center = {
+    textAlign: "center",
+  };
 
-    return ( 
-        <Layout>
-            <div style={center}>
-                <h1>404</h1>
-                <p>This is the 404 Page.</p>
-            </div>
-        </Layout>
-     );
-}
- 
+  return (
+    <Layout>
+      <div style={center}>
+        <h1>404</h1>
+        <p>Error 404 page was not found.</p>
+      </div>
+    </Layout>
+  );
+};
+
 export default NotFound;
